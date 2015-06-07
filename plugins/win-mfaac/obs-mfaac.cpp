64a12c66ddfe81adf74bea4fac5276371baefce0
John R. Bradley <jrb@turrettech.com>
2015-06-07 09:25:16 -0500

    Fix tabs, cleanup and remove warnings

--- plugins/win-mfaac/obs-mfaac.cpp
+++ plugins/win-mfaac/obs-mfaac.cpp
@@ -6,7 +6,6 @@
 #include <Windows.h>
 #undef WIN32_MEAN_AND_LEAN
 
-#include <Shlwapi.h>
 #include <mfapi.h>
 #include <mfidl.h>
 #include <Mferror.h>
@@ -19,10 +18,12 @@ using namespace Microsoft::WRL;
 struct mfaac_encoder {
 	obs_encoder_t *encoder;
 
-    int channels, sample_rate, bits_per_sample;
+	int channels;
+	int sample_rate;
+	int bits_per_sample;
 
-    ComPtr<IMFTransform> transform;
-    ComPtr<IMFSample> output_sample;
+	ComPtr<IMFTransform> transform;
+	ComPtr<IMFSample> output_sample;
 
 	uint64_t total_samples;
 
@@ -30,11 +31,8 @@ struct mfaac_encoder {
 
 	uint8_t *packet_buffer;
 	int packet_buffer_size;
-    UINT16 header;
-    uint8_t header5[5];
+	uint8_t header[5];
 };
-
-bool mfaac_extra_data(void *data, uint8_t **extra_data, size_t *size);
 
 static const char *mfaac_getname(void)
 {
@@ -65,86 +63,87 @@ static void mfaac_defaults(obs_data_t *settings)
 	}
 
 static HRESULT CreateMediaTypes(ComPtr<IMFMediaType> &i, 
-        ComPtr<IMFMediaType> &o, unsigned int sampleRate, 
-        unsigned int channels, unsigned int bitsPerSample)
+		ComPtr<IMFMediaType> &o, unsigned int sampleRate, 
+		unsigned int channels, unsigned int bitsPerSample)
 {
-    HRESULT hr;
-    HRC(MFCreateMediaType(&i));
-    HRC(MFCreateMediaType(&o));
+	HRESULT hr;
+	HRC(MFCreateMediaType(&i));
+	HRC(MFCreateMediaType(&o));
 
-    HRC(i->SetGUID(MF_MT_MAJOR_TYPE, MFMediaType_Audio));
-    HRC(i->SetGUID(MF_MT_SUBTYPE, MFAudioFormat_PCM));
-    HRC(i->SetUINT32(MF_MT_AUDIO_BITS_PER_SAMPLE, bitsPerSample));
-    HRC(i->SetUINT32(MF_MT_AUDIO_SAMPLES_PER_SECOND, sampleRate));
-    HRC(i->SetUINT32(MF_MT_AUDIO_NUM_CHANNELS, channels));
+	HRC(i->SetGUID(MF_MT_MAJOR_TYPE, MFMediaType_Audio));
+	HRC(i->SetGUID(MF_MT_SUBTYPE, MFAudioFormat_PCM));
+	HRC(i->SetUINT32(MF_MT_AUDIO_BITS_PER_SAMPLE, bitsPerSample));
+	HRC(i->SetUINT32(MF_MT_AUDIO_SAMPLES_PER_SECOND, sampleRate));
+	HRC(i->SetUINT32(MF_MT_AUDIO_NUM_CHANNELS, channels));
 
-    HRC(o->SetGUID(MF_MT_MAJOR_TYPE, MFMediaType_Audio));
-    HRC(o->SetGUID(MF_MT_SUBTYPE, MFAudioFormat_AAC));
-    HRC(o->SetUINT32(MF_MT_AUDIO_BITS_PER_SAMPLE, bitsPerSample));
-    HRC(o->SetUINT32(MF_MT_AUDIO_SAMPLES_PER_SECOND, sampleRate));
-    HRC(o->SetUINT32(MF_MT_AUDIO_NUM_CHANNELS, channels));
+	HRC(o->SetGUID(MF_MT_MAJOR_TYPE, MFMediaType_Audio));
+	HRC(o->SetGUID(MF_MT_SUBTYPE, MFAudioFormat_AAC));
+	HRC(o->SetUINT32(MF_MT_AUDIO_BITS_PER_SAMPLE, bitsPerSample));
+	HRC(o->SetUINT32(MF_MT_AUDIO_SAMPLES_PER_SECOND, sampleRate));
+	HRC(o->SetUINT32(MF_MT_AUDIO_NUM_CHANNELS, channels));
 
-    return S_OK;
+	return S_OK;
 fail:
-    return hr;
+	return hr;
 }
 
 static HRESULT CreateEmptySample(ComPtr<IMFSample> &sample, int length)
 {
-    HRESULT hr;
-    ComPtr<IMFMediaBuffer> mediaBuffer;
+	HRESULT hr;
+	ComPtr<IMFMediaBuffer> mediaBuffer;
 
-    HRC(MFCreateSample(&sample));
-    HRC(MFCreateMemoryBuffer(length, &mediaBuffer));
-    HRC(sample->AddBuffer(mediaBuffer.Get()));
-    return S_OK;
+	HRC(MFCreateSample(&sample));
+	HRC(MFCreateMemoryBuffer(length, &mediaBuffer));
+	HRC(sample->AddBuffer(mediaBuffer.Get()));
+	return S_OK;
 fail:
-    return hr;
+	return hr;
 }
 
 static void *mfaac_create(obs_data_t *settings, obs_encoder_t *encoder)
 {
-	bool hasFdkHandle = false;
-    int bitrate = (int) obs_data_get_int(settings, "bitrate");
-    int bytes_per_sec = (bitrate * 1000) / 8;
-    
-	int mode = 0;
-    HRESULT hr;
+	int bitrate = (int) obs_data_get_int(settings, "bitrate");
+	int bytes_per_sec = (bitrate * 1000) / 8;
 
-    ComPtr<IMFTransform> transform;
-    ComPtr<IMFMediaType> inputType, outputType;
-    ComPtr<IMFMediaBuffer> outputBuffer;
-    ComPtr<IMFSample> outputSample;
+	HRESULT hr;
 
-    std::unique_ptr<mfaac_encoder> enc(new mfaac_encoder());
+	ComPtr<IMFTransform> transform;
+	ComPtr<IMFMediaType> inputType, outputType;
+	ComPtr<IMFMediaBuffer> outputBuffer;
+	ComPtr<IMFSample> outputSample;
 
-    if (!bytes_per_sec) {
+	std::unique_ptr<mfaac_encoder> enc(new mfaac_encoder());
+
+	if (!bytes_per_sec) {
 		blog(LOG_ERROR, "Invalid bitrate");
 		return NULL;
 	}
 
-    enc->encoder = encoder;
+	enc->encoder = encoder;
 	
-    audio_t *audio = obs_encoder_audio(encoder);
-    enc->channels = (int)audio_output_get_channels(audio);
+	audio_t *audio = obs_encoder_audio(encoder);
+	enc->channels = (int)audio_output_get_channels(audio);
 	enc->sample_rate = audio_output_get_sample_rate(audio);
-    enc->bits_per_sample = 16;
+	enc->bits_per_sample = 16;
 
 	if (enc->channels > 2) {
 		blog(LOG_ERROR, "Invalid channel count");
 		goto fail;
 	}
 
-    HRC(CoCreateInstance(CLSID_AACMFTEncoder, NULL, CLSCTX_INPROC_SERVER, IID_PPV_ARGS(&transform)));
-    HRC(CreateMediaTypes(inputType, outputType, enc->sample_rate, enc->channels, enc->bits_per_sample));
-    HRC(outputType->SetUINT32(MF_MT_AUDIO_AVG_BYTES_PER_SECOND, bytes_per_sec));
-    HRC(transform->SetInputType(0, inputType.Get(), 0));
-    HRC(transform->SetOutputType(0, outputType.Get(), 0));
+	HRC(CoCreateInstance(CLSID_AACMFTEncoder, NULL, CLSCTX_INPROC_SERVER,
+			IID_PPV_ARGS(&transform)));
+	HRC(CreateMediaTypes(inputType, outputType, enc->sample_rate,
+			enc->channels, enc->bits_per_sample));
+	HRC(outputType->SetUINT32(MF_MT_AUDIO_AVG_BYTES_PER_SECOND,
+			bytes_per_sec));
+	HRC(transform->SetInputType(0, inputType.Get(), 0));
+	HRC(transform->SetOutputType(0, outputType.Get(), 0));
 
-    hasFdkHandle = true;
-
-    HRC(transform->ProcessMessage(MFT_MESSAGE_NOTIFY_BEGIN_STREAMING, NULL));
-    HRC(transform->ProcessMessage(MFT_MESSAGE_NOTIFY_START_OF_STREAM, NULL));
+	HRC(transform->ProcessMessage(MFT_MESSAGE_NOTIFY_BEGIN_STREAMING,
+			NULL));
+	HRC(transform->ProcessMessage(MFT_MESSAGE_NOTIFY_START_OF_STREAM,
+			NULL));
 
 	enc->frame_size_bytes = 1024 * 2 * enc->channels;
 	enc->packet_buffer_size = enc->channels * 768;
@@ -153,26 +152,19 @@ static void *mfaac_create(obs_data_t *settings, obs_encoder_t *encoder)
 
 	enc->packet_buffer = (uint8_t *)bmalloc(enc->packet_buffer_size);
 
-    HRC(CreateEmptySample(outputSample, enc->packet_buffer_size));
-    enc->output_sample = outputSample;
+	HRC(CreateEmptySample(outputSample, enc->packet_buffer_size));
+	enc->output_sample = outputSample;
 
 	blog(LOG_INFO, "mfaac_aac encoder created");
 	blog(LOG_INFO, "mfaac_aac bitrate: %d, channels: %d",
 			bitrate, enc->channels);
 
-    UINT8 *data;
-    size_t size;
-    
-    enc->transform = transform;
-    
-    mfaac_extra_data(enc.get(), &data, &size);
+	enc->transform = transform;
 
-    return enc.release();
+	return enc.release();
 
 fail:
-
 	blog(LOG_WARNING, "mfaac encoder creation failed");
-
 	return 0;
 }
 
@@ -181,108 +173,109 @@ static void mfaac_destroy(void *data)
 	mfaac_encoder *enc = (mfaac_encoder *)data;
 
 	bfree(enc->packet_buffer);
-
-    delete data;
+	delete data;
 
 	blog(LOG_INFO, "mfaac encoder destroyed");
 }
 
 static bool mfaac_encode(void *data, struct encoder_frame *frame,
-                          struct encoder_packet *packet, bool *received_packet)
+		struct encoder_packet *packet, bool *received_packet)
 {
 	mfaac_encoder *enc = (mfaac_encoder *)data;
-    HRESULT hr;
+	HRESULT hr;
 
-    ComPtr<IMFSample> sample;
-    ComPtr<IMFMediaBuffer> mediaBuffer;
+	ComPtr<IMFSample> sample;
+	ComPtr<IMFMediaBuffer> mediaBuffer;
+	BYTE *mediaBufferData;
+	DWORD mediaBufferSize;
+	MFT_OUTPUT_DATA_BUFFER output = { 0 };
+	UINT32 samples;
+	INT64 samplePts;
+	float sampleDur;
+	DWORD status;
 
-    HRC(CreateEmptySample(sample, frame->linesize[0]));
-    HRC(sample->GetBufferByIndex(0, &mediaBuffer));
-    
-    BYTE *mediaBufferData;
-    HRC(mediaBuffer->Lock(&mediaBufferData, NULL, NULL));
-    memcpy(mediaBufferData, frame->data[0], frame->linesize[0]);
-    HRC(mediaBuffer->Unlock());
-    HRC(mediaBuffer->SetCurrentLength(frame->linesize[0]));
+	HRC(CreateEmptySample(sample, frame->linesize[0]));
+	HRC(sample->GetBufferByIndex(0, &mediaBuffer));
 
-    UINT32 samples = frame->linesize[0] / enc->channels / (enc->bits_per_sample / 8); 
-    INT64 samplePts = frame->pts / 100;
-    float sampleDur = (float)enc->sample_rate / enc->channels / samples;
-    sample->SetSampleTime(samplePts);
-    sample->SetSampleDuration(10000 * sampleDur);
-    DWORD inputStatus;
-    HRC(enc->transform->GetInputStatus(0, &inputStatus));
-    
-    hr = enc->transform->ProcessInput(0, sample.Get(), 0);
-    if (hr == MF_E_NOTACCEPTING)
-        return false;
+	HRC(mediaBuffer->Lock(&mediaBufferData, NULL, NULL));
+	memcpy(mediaBufferData, frame->data[0], frame->linesize[0]);
+	HRC(mediaBuffer->Unlock());
+	HRC(mediaBuffer->SetCurrentLength(frame->linesize[0]));
 
-    sample.Reset();
-    mediaBuffer.Reset();
+	samples = frame->linesize[0] / enc->channels /
+		(enc->bits_per_sample / 8);
+	samplePts = frame->pts / 100;
+	sampleDur = (float)enc->sample_rate / enc->channels / samples;
+	sample->SetSampleTime(samplePts);
+	sample->SetSampleDuration((LONGLONG)(10000 * sampleDur));
 
-    sample = enc->output_sample;
-    HRC(sample->GetBufferByIndex(0, &mediaBuffer));
+	hr = enc->transform->ProcessInput(0, sample.Get(), 0);
+	if (hr == MF_E_NOTACCEPTING)
+		return false;
 
-    MFT_OUTPUT_DATA_BUFFER output = { 0 };
-    output.pSample = sample.Get();
+	sample.Reset();
+	mediaBuffer.Reset();
 
-    DWORD status;
-    hr = enc->transform->ProcessOutput(0, 1, &output, &status);
-    if (hr == MF_E_TRANSFORM_NEED_MORE_INPUT)
-        return true;
-    
-    DWORD mediaBufferSize;
-    HRC(mediaBuffer->Lock(&mediaBufferData, NULL, &mediaBufferSize));
-    memcpy(enc->packet_buffer, mediaBufferData, mediaBufferSize);
-    HRC(mediaBuffer->Unlock());
-    HRC(mediaBuffer->SetCurrentLength(frame->linesize[0]));
+	sample = enc->output_sample;
+	HRC(sample->GetBufferByIndex(0, &mediaBuffer));
 
-    HRC(sample->GetSampleTime(&samplePts));
+	output.pSample = sample.Get();
 
-    packet->pts = samplePts * 100;
-    packet->dts = packet->pts;
+	hr = enc->transform->ProcessOutput(0, 1, &output, &status);
+	if (hr == MF_E_TRANSFORM_NEED_MORE_INPUT)
+		return true;
+
+	HRC(mediaBuffer->Lock(&mediaBufferData, NULL, &mediaBufferSize));
+	memcpy(enc->packet_buffer, mediaBufferData, mediaBufferSize);
+	HRC(mediaBuffer->Unlock());
+	HRC(mediaBuffer->SetCurrentLength(frame->linesize[0]));
+
+	HRC(sample->GetSampleTime(&samplePts));
+
+	packet->pts = samplePts * 100;
+	packet->dts = packet->pts;
 	packet->data = enc->packet_buffer;
 	packet->size = mediaBufferSize;
 	packet->type = OBS_ENCODER_AUDIO;
 	packet->timebase_num = 1;
 	packet->timebase_den = enc->sample_rate;
 
-    return *received_packet = true;
+	return *received_packet = true;
 fail:
-    return false;
+	return false;
 }
 
 static bool mfaac_extra_data(void *data, uint8_t **extra_data, size_t *size)
 {
 	mfaac_encoder *enc = (mfaac_encoder *)data;
 
-    uint16_t *header = (uint16_t *) enc->header5;
+	uint16_t *header = (uint16_t *) enc->header;
 
 #define SWAPU16(x) (x>>8) | (x<<8)
-    // LC Profile
-    // XXXX X... .... ....
-    *header  = 2 << 11;
-    // Sample Index (3=48, 4=44.1)
-    // .... .XXX X... ....
-    *header |= enc->sample_rate == 48000 ? 3 : 4 << 7;
-    // Channels
-    // .... .... .XXX X...
-    *header |= enc->channels << 3;
-    *header = SWAPU16(*header);
+	// LC Profile
+	// XXXX X... .... ....
+	*header  = 2 << 11;
+	// Sample Index (3=48, 4=44.1)
+	// .... .XXX X... ....
+	*header |= enc->sample_rate == 48000 ? 3 : 4 << 7;
+	// Channels
+	// .... .... .XXX X...
+	*header |= enc->channels << 3;
+	*header = SWAPU16(*header);
 
-    // Extensions
-    header++;
-    *header = 0x2b7 << 5;
-    // LC Profile
-    *header |= 2;
-    *header = SWAPU16(*header);
+	// Extensions
+	header++;
+	*header = 0x2b7 << 5;
+	// LC Profile
+	*header |= 2;
+	*header = SWAPU16(*header);
 
-    enc->header5[4] = 0;
+	enc->header[4] = 0;
 #undef SWAPU16
 
-    *extra_data = enc->header5;
-    *size = 3;
-    return true;
+	*extra_data = enc->header;
+	*size = 3;
+	return true;
 }
 
 static void mfaac_audio_info(void *data, struct audio_convert_info *info)
@@ -293,32 +286,33 @@ static void mfaac_audio_info(void *data, struct audio_convert_info *info)
 
 static size_t mfaac_frame_size(void *data)
 {
-    // MF Frame size is always 1024
-    return 1024;
+	UNUSED_PARAMETER(data);
+	// MF Frame size is always 1024
+	return 1024;
 }
 
 void RegisterMFAACEncoder()
 {
-    obs_encoder_info info = { 0 };
-    info.id = "mf_aac";
-    info.type = OBS_ENCODER_AUDIO;
-    info.codec = "AAC";
-    info.get_name = mfaac_getname;
-    info.create = mfaac_create;
-    info.destroy = mfaac_destroy;
-    info.encode = mfaac_encode;
-    info.get_frame_size = mfaac_frame_size;
-    info.get_defaults = mfaac_defaults;
-    info.get_properties = mfaac_properties;
-    info.get_extra_data = mfaac_extra_data;
-    info.get_audio_info = mfaac_audio_info;
+	obs_encoder_info info = { 0 };
+	info.id = "mf_aac";
+	info.type = OBS_ENCODER_AUDIO;
+	info.codec = "AAC";
+	info.get_name = mfaac_getname;
+	info.create = mfaac_create;
+	info.destroy = mfaac_destroy;
+	info.encode = mfaac_encode;
+	info.get_frame_size = mfaac_frame_size;
+	info.get_defaults = mfaac_defaults;
+	info.get_properties = mfaac_properties;
+	info.get_extra_data = mfaac_extra_data;
+	info.get_audio_info = mfaac_audio_info;
 
-    obs_register_encoder(&info);
+	obs_register_encoder(&info);
 }
 
 bool obs_module_load(void)
 {
-    RegisterMFAACEncoder();
+	RegisterMFAACEncoder();
 	return true;
 }
 
